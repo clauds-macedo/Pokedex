79cb7ee07afd227f459ed37d19a42bf693b136c7
clauds-macedo <67011538+clauds-macedo@users.noreply.github.com>
2021-12-30 11:37:12 -0300

    added splash screens while pokemon list is loading

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,26 +1,32 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, Image, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from "react-native";
 import { styles } from "./styles";
 
-import Pokebola from "../../assets/Pokeball.svg";
+
 import Pokeball from "../../assets/Pokebola";
 import { theme } from "../../global/theme";
 import { Ionicons } from "@expo/vector-icons";
 import api from "../../services/api";
 import IPokeData from "../../services/IPokeData";
+import SplashScreen from "../splashScreen";
 
 export default function Home() {
   const [pokeData, setPokeData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
 
   async function getPokeDataFirstGen() {
-    
     for (var pokemon = 1; pokemon < 50; pokemon++) {
       const response = await api.get(`pokemon/${pokemon}`);
       pokeData.push(response.data as never);
     }
-    setIsLoading(!isLoading)
-    
+    setIsLoading(!isLoading);
   }
 
   useEffect(() => {
@@ -29,41 +35,60 @@ export default function Home() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <View style={styles.header}>
-          <Pokeball />
-          <Text style={styles.textLogo}>Pokédex</Text>
-        </View>
-        <Ionicons name="filter" size={32} color="black" />
-      </View>
-      <TextInput style={styles.search} placeholder="Procurar" />
-      <ScrollView showsVerticalScrollIndicator={false}>
-          <View style={styles.pokemonContainer}>
-            {!isLoading ? (
-              pokeData.map((poke: IPokeData, key: number) => {
-                const pokeColor = theme.types[(poke.types[0]['type'].name)]
+      {!isLoading ?
+        <>
+          <View style={styles.headerContainer}>
+            <View style={styles.header}>
+              <Pokeball />
+              <Text style={styles.textLogo}>Pokédex</Text>
+            </View>
+            <Ionicons name="filter" size={32} color="black" />
+          </View>
+          <TextInput style={styles.search} placeholder="Procurar" />
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={styles.pokemonContainer}>
+              {pokeData.map((poke: IPokeData, key: number) => {
+                const pokeColor = theme.types[poke.types[0]["type"].name];
                 return (
-                  <View style={[styles.pokemonCard]} key={key}
-                  >
-                    <TouchableOpacity activeOpacity={0.7}
-                    onPress={() => {console.log(poke.id)}}
+                  <View style={[styles.pokemonCard]} key={key}>
+                    <TouchableOpacity
+                      activeOpacity={0.7}
+                      onPress={() => {
+                        console.log(poke.id);
+                      }}
                     >
-                        <View style={[styles.pokemon, , {borderColor: `${pokeColor}`}]}>
-                            <Image source={{uri: `https://cdn.traction.one/pokedex/pokemon/${poke.id}.png`}}
-                            style={{width: 100, height: 100}}/>
-                          <View style={[styles.rectangle, {backgroundColor: `${pokeColor}`}]}>
-                              <Text style={styles.pokeName}>{poke.name}</Text>
-                          </View>
+                      <View
+                        style={[
+                          styles.pokemon,
+                          ,
+                          { borderColor: `${pokeColor}` },
+                        ]}
+                      >
+                        <Image
+                          source={{
+                            uri: `https://cdn.traction.one/pokedex/pokemon/${poke.id}.png`,
+                          }}
+                          style={{ width: 100, height: 100 }}
+                        />
+                        <View
+                          style={[
+                            styles.rectangle,
+                            { backgroundColor: `${pokeColor}` },
+                          ]}
+                        >
+                          <Text style={styles.pokeName}>{poke.name}</Text>
                         </View>
+                      </View>
                     </TouchableOpacity>
                   </View>
                 );
-              })
-            ) : (
-              <Text style={{fontSize: 40}}>Loading</Text>
-            )}
-          </View>
-      </ScrollView>
+              })}
+            </View>
+          </ScrollView>
+        </>
+      :
+      <SplashScreen/>
+      }
     </View>
   );
 }
