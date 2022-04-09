5634ec0c45edfbdc648aae66c2f84ff119b5a012
withoutspaces <dms@ic.ufal.br>
2022-04-09 17:07:45 -0300

    removing the generations and fixing bugs

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -8,157 +8,82 @@ import {
   Image,
 } from "react-native";
 import { styles } from "./styles";
-import Modal from "react-native-modal";
-import Header from "../../components/Header";
+import { theme } from "../../global/theme";
 
+import Header from "../../components/Header";
 import IPokeData from "../../services/IPokeData";
 import PokeBox from "../../components/PokeBox";
-import { Ionicons } from "@expo/vector-icons";
-import { theme } from "../../global/theme";
-import SplashScreen from "../Splash";
 import api from "../../services/api";
-import Loading from "../Loading";
+
+import LottieView from 'lottie-react-native';
+import squirtle from '../../assets/lotties/splash-screen.json'
+
+const START = 1
+const END = 151
+
 
 export default function Home() {
   const [initialPokeData, setInitialPokeData] = useState([]);
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [isFirstLoad, setIsFirstLoad] = useState(true);
-  const [search, setSearch] = useState<string>("");
-  const [starterNum, setStarterNum] = useState(1);
-  const [isLoaded, setIsLoaded] = useState(false);
   const [pokeData, setPokeData] = useState([]);
+
+  const [search, setSearch] = useState<string>("");
+
+  const [isLoaded, setIsLoaded] = useState(false);
   const [ordered, setOrdered] = useState(false)
-  const [endNum, setEndNum] = useState(75);
-  const [gen, setGen] = useState(1);
-  const gens = [1, 2, 3, 4, 5, 6, 7];
 
-  const generationIDS = [
-    { start: 1, end: 151, id: 0 },
-    { start: 152, end: 251, id: 1 },
-    { start: 252, end: 386, id: 2 },
-    { start: 387, end: 493, id: 3 },
-    { start: 494, end: 649, id: 4 },
-    { start: 650, end: 720, id: 5 },
-    { start: 722, end: 807, id: 6 },
-  ];
-
-  async function getPokeData(start: number, end: number) {
-    for (var pokemon = start; pokemon <= end; pokemon++) {
-      const response = await api.get(`pokemon/${pokemon}`);
-      pokeData.push(response.data as never);
+  async function getPokeData() {
+    const data: any = new Array
+    for (var pokemon = START; pokemon <= END; pokemon++) {
+      await api.get(`pokemon/${pokemon}`)
+        .then((response) => {
+          data.push(response.data)
+        })
+        .catch(err => {
+          console.log('Opa, deu um erro ' + err)
+        })
     }
+    setPokeData(data)
     setIsLoaded(!isLoaded);
   }
 
-  const changeModalVisibility = () => {
-    setIsModalVisible(!isModalVisible);
-  };
-
-  const splicePokemons = () => {
-    setPokeData([]);
-  };
-
-  const loadPrevious = () => {
-    if (starterNum !== generationIDS[gen - 1].start) {
-      splicePokemons();
-      setStarterNum(starterNum - 20);
-      setEndNum(endNum - 20);
-      setIsLoaded(false);
-    }
-  };
-
-  const loadNext = () => {
-    if (
-      endNum <= generationIDS[gen - 1].end &&
-      endNum !== generationIDS[gen - 1].end
-    ) {
-      splicePokemons();
-      setStarterNum(starterNum + 20);
-      starterNum + 41 < generationIDS[gen - 1].end
-        ? setEndNum(starterNum + 40)
-        : setEndNum(generationIDS[gen - 1].end);
-      setIsLoaded(false);
-    }
-  };
-
-  const handleChangeGen = () => {
-    splicePokemons();
-    setIsModalVisible(!isModalVisible);
-    setIsLoaded(false);
-    setStarterNum(generationIDS[gen - 1].start);
-    setEndNum(generationIDS[gen - 1].start + 20);
-  };
-
   useEffect(() => {
     if (isLoaded === false) {
-      getPokeData(starterNum, endNum);
-      setIsFirstLoad(false);
-      setGen(gen);
+      getPokeData();
     }
-  }, [isLoaded, starterNum, endNum, setGen]);
+  }, [isLoaded]);
 
   const sortPokemons = () => {
     setInitialPokeData(pokeData);
-    let newData = [...initialPokeData];
-
-    newData.sort((a, b) => a.name < b.name ? -1 : 1);
-
+    let newData = [...pokeData];
+    newData.sort((a: { name: string }, b: { name: string }) => a.name < b.name ? -1 : 1);
     return newData;
   }
 
   const handleSortByNameButton = () => {
-    if(!ordered) {
-      setOrdered(true);
+    if (!ordered) {
       setPokeData(sortPokemons);
+      setOrdered(true);
     } else {
       setPokeData(initialPokeData);
       setOrdered(false);
     }
-    
   }
 
   return (
     <>
-      {isLoaded ? (
-        <View style={styles.container}>
-          <Header 
-            function={() => {handleSortByNameButton()}} 
-            ordered={ordered}
+      <View style={styles.container}>
+        <Header
+          function={() => { handleSortByNameButton() }}
+          ordered={ordered}
+        />
+        <View style={styles.searchBox}>
+          <TextInput
+            style={styles.search}
+            placeholder="Procurar"
+            onChangeText={(textFromEvent) => setSearch(textFromEvent)}
           />
-          <View style={styles.searchBox}>
-            <TextInput
-              style={styles.search}
-              placeholder="Procurar"
-              onChangeText={(textFromEvent) => setSearch(textFromEvent)}
-            />
-          </View>
-
-          <View style={styles.changePage}>
-            <TouchableOpacity>
-              <Ionicons
-                name="arrow-back-outline"
-                size={32}
-                color={
-                  generationIDS[gen - 1].start === starterNum
-                    ? "#e0e0e0"
-                    : "black"
-                }
-                onPress={() => {
-                  loadPrevious();
-                }}
-              />
-            </TouchableOpacity>
-            <Ionicons
-              name="arrow-forward-outline"
-              size={32}
-              color={
-                generationIDS[gen - 1].end === endNum ? "#e0e0e0" : "black"
-              }
-              onPress={() => {
-                loadNext();
-              }}
-            />
-          </View>
+        </View>
+        {isLoaded ? (
           <ScrollView showsVerticalScrollIndicator={false}>
             <View style={styles.pokemonContainer}>
               {pokeData.map((poke: IPokeData, key: number) => {
@@ -168,6 +93,8 @@ export default function Home() {
                     <PokeBox
                       key={key}
                       img={`https://cdn.traction.one/pokedex/pokemon/${poke.id}.png`}
+                      // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${poke.id}.png
+                      // https://cdn.traction.one/pokedex/pokemon/${poke.id}.png
                       bgColor={pokeColor}
                       name={poke.name}
                       id={poke.id}
@@ -176,45 +103,19 @@ export default function Home() {
                 }
               })}
             </View>
-
-            {/* <Modal
-              isVisible={isModalVisible}
-              style={{ display: "flex", alignItems: "center" }}
-              onBackdropPress={() => setIsModalVisible(!isModalVisible)}
-            >
-              <View style={styles.modalView}>
-                <Text style={styles.modalText}>Filter</Text>
-                <View style={styles.genChoose}>
-                  {gens.map((uGen, key) => {
-                    return (
-                      <TouchableOpacity
-                        style={styles.genButton}
-                        key={key}
-                        onPress={() => {
-                          setGen(uGen);
-                        }}
-                      >
-                        <Text>{uGen}</Text>
-                      </TouchableOpacity>
-                    );
-                  })}
-                </View>
-                <TouchableOpacity
-                  onPress={() => {
-                    handleChangeGen();
-                  }}
-                >
-                  <Text>Apply</Text>
-                </TouchableOpacity>
-              </View>
-            </Modal> */}
           </ScrollView>
-        </View>
-      ) : !isLoaded ? (
-        <Loading />
-      ) : (
-        <SplashScreen />
-      )}
+        ) : (
+          // <Loading />
+          <View style={styles.containerAnimation}>
+            <LottieView
+              style={styles.animation}
+              source={squirtle}
+              autoPlay
+              loop
+            />
+          </View>
+        )}
+      </View>
     </>
   );
 }
