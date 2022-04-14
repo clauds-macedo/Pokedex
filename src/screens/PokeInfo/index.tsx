9d379e5b779df1beee27b29bf9187c1da9704aeb
withoutspaces <dms@ic.ufal.br>
2022-04-14 13:09:59 -0300

    adding Pokeinfo screen

---
+++ src/screens/PokeInfo/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+
+export default function PokeInfo() {
+  return (
+    <View>
+      <Text>Hello, world!</Text>
+    </View>
+  )
+}
