7c0ad9fc26e574e6970b367e8628d83828ec8c7f
withoutspaces <dms@ic.ufal.br>
2022-04-14 13:10:38 -0300

    adding the background component

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import Background from './src/components/Background';
+
 import { StatusBar } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Routes } from './src/routes';
+import { theme } from './src/global/theme';
+
 
 export default function App() {
 
@@ -12,14 +16,14 @@ export default function App() {
   });
 
   return (
-    <>
+    <Background>
       <StatusBar
         barStyle='light-content'
         backgroundColor='transparent'
         translucent
       />
       {fontsLoaded ? <Routes /> : null}
-    </>
+    </Background>
   );
 }
 
