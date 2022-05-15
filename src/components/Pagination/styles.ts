a1f193ee5be252c648e525746a2d9b451278495f
withoutspaces <dms@ic.ufal.br>
2022-05-15 16:34:03 -0300

    adding pagination component

---
+++ src/components/Pagination/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/theme";
+
+export const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 10,
+
+  }
+})
