a78f3fd6a64117cbf95a1f5b383657f3fe774141
withoutspaces <dms@ic.ufal.br>
2023-10-20 10:24:50 -0300

    refactor: keys of the "types" obj, now are strings

--- src/global/theme.ts
+++ src/global/theme.ts
@@ -1,4 +1,4 @@
-export const theme: any = {
+export const theme = {
     colors: {
         background: "#F7F7F7",
         gray250: "#E0E0E0",
@@ -8,24 +8,24 @@ export const theme: any = {
     },
 
     types: {
-        rock: "#B69E31",
-        ghost: "#70559B",
-        steel: "#B7B9D0",
-        water: "#6493EB",
-        grass: "#74CB48",
-        psychic: "#FB5584",
-        ice: "#9AD6DF",
-        dark: "#75574C",
-        fairy: "#E69EAC",
-        normal: "#AAA67F",
-        fighting: "#C12239",
-        flying: "#A891EC",
-        poison: "#A43E9E",
-        ground: "#DEC16B",
-        bug: "#A7B723",
-        fire: "#F57D31",
-        electric: "#F9CF30",
-        dragon: "#7037FF"
+        "rock": "#B69E31",
+        "ghost": "#70559B",
+        "steel": "#B7B9D0",
+        "water": "#6493EB",
+        "grass": "#74CB48",
+        "psychic": "#FB5584",
+        "ice": "#9AD6DF",
+        "dark": "#75574C",
+        "fairy": "#E69EAC",
+        "normal": "#AAA67F",
+        "fighting": "#C12239",
+        "flying": "#A891EC",
+        "poison": "#A43E9E",
+        "ground": "#DEC16B",
+        "bug": "#A7B723",
+        "fire": "#F57D31",
+        "electric": "#F9CF30",
+        "dragon": "#7037FF"
     },
 
     fonts: {
