00d659281a9a013f3c00bc6f3eea602c70e4b886
withoutspaces <dms@ic.ufal.br>
2023-10-20 10:32:47 -0300

    fix: error in the api path to the image

--- src/services/IPokeData.ts
+++ src/services/IPokeData.ts
@@ -1,99 +1,48 @@
 export default interface IPokeData {
+  abilities: Array<{ ability: { name: string; url: string }, is_hidden: boolean, slot: number}>;
+  base_experience: number;
+  forms: Array<{ name: string; url: string }>;
+  height: number;
+  held_items: [];
+  id: number;
+  is_default: boolean;
+  location_area_encounters: string;
+  name: string;
+  order: number;
+  past_types: [];
+  species: {
+    name: string;
+    url: string;
+  };
 
-    abilities: [
-        {
-            ability: {
-                name: string,
-                url: string,
-            },
-            is_hidden: boolean,
-            slot: number
-        }
-    ],
+  sprites: {
+    other: {
+      dream_world: {
+        front_default: string;
+      };
+      "official-artwork": {
+        front_default: string;
+      };
+    };
+  };
 
-    base_experience: number,
+  stats: Array<{
+    base_stat: number;
+    effort: number;
+    stat: {
+      name: string;
+      url: string;
+    };
+  }>;
 
-    forms: [
-        {
-            name: string,
-            url: string
-        }
-    ],
-
-    game_indices: [
-        {
-            game_index: 154,
-            version: {
-                name: string,
-                url: string
-            }
-        }
-    ],
-
-    height: number,
-    held_items: [],
-    id: number,
-    is_default: boolean,
-    location_area_encounters: string,
-
-    moves: [
-        {
-            move: {
-                name: string,
-                url: string
-            },
-            version_group_details: [
-                {
-                    level_learned_at: number,
-                    move_learn_method: {
-                        name: string,
-                        url: string
-                    },
-                    version_group: {
-                        name: string,
-                        url: string
-                    }
-                }
-            ]
-        }
-    ],
-
-    name: 'string',
-    order: number,
-    past_types: [],
-    species: {
-        name: string,
-        url: string
+  types: [
+    {
+      slot: number;
+      type: {
+        name: string;
+        url: string;
+      };
     }
-
-    sprites: {
-        other: {
-            official_artwork: {
-                front_default: string
-            }
-        }
-    }
-
-    stats: [
-        {
-            base_stat: number,
-            effort: number,
-            stat: {
-                name: string,
-                url: string
-            }
-        }
-    ],
-
-    types: [
-        {
-            slot: number,
-            type: {
-                name: string,
-                url: string
-            }
-        }
-    ]
-    weight: number
-
+  ];
+  weight: number;
 }
