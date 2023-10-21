498cd2f479c3a6b45f036d7b85d7516132a00f5d
withoutspaces <dms@ic.ufal.br>
2023-10-21 12:24:53 -0300

    feat: adding error screen

--- src/screens/Home/components/Error/index.tsx
+++ src/screens/Home/components/Error/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { styles } from "./styles";
+import errorAnimation from '../../../../assets/lotties/errorScreen.json'
+import LottieView from 'lottie-react-native';
+import Button from "../../../../components/Button";
 
 export default function Error() {
     return (
         <View style={styles.container}>
-            <Text>Opa, algo deu errado</Text>
+            <LottieView 
+                source={errorAnimation}
+                autoPlay
+                loop
+                style={styles.animation}
+            />
+            <Text style={styles.text}>Opss...{'\n'}algo deu errado</Text>
+            <Button>Tentar Novamente</Button>
         </View>
     )
 }
--- src/screens/Home/components/Error/styles.ts
+++ src/screens/Home/components/Error/styles.ts
@@ -1,9 +1,20 @@
 import { StyleSheet } from "react-native";
+import { theme } from "../../../../global/theme";
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "center",
+        marginBottom: 100
+    },
+    animation: {
+        width: "100%"
+    },
+    text: {
+        fontFamily: theme.fonts.title700,
+        color: theme.colors.darkGray,
+        fontSize: 26,
+        textAlign: "center"
     }
 })
