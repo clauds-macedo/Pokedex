9be169ed43454ec659ac2feeff255997b1f5ac6c
withoutspaces <dms@ic.ufal.br>
2023-10-21 12:21:55 -0300

    styles:prettier formatting

--- src/components/PokeBox/index.tsx
+++ src/components/PokeBox/index.tsx
@@ -1,47 +1,44 @@
 import React, { useContext } from "react";
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import { context } from "../../context";
 import { styles } from "./styles";
 import IPokeData from "../../services/IPokeData";
-import { theme } from "../../global/theme";
 
 interface PokeBoxProps extends IPokeData {
-    onPress?: () => void
-    theme?: ThemeType
+  onPress?: () => void;
+  theme: ThemeType;
 }
 
 interface ThemeType {
-    types: Record<string, string>
+  types: Record<string, string>;
 }
 
-export default function PokeBox({ sprites, name, id, types, onPress, theme }: PokeBoxProps) {
+export default function PokeBox({
+  sprites,
+  name,
+  id,
+  types,
+  onPress,
+  theme,
+}: PokeBoxProps) {
+  const bgColor = theme.types[types[0].type.name];
 
-    const bgColor = theme.types[types[0].type.name]
-    
-    const {front_default} = sprites.other["official-artwork"]
+  const { front_default } = sprites.other["official-artwork"];
 
-    return (
-        <View style={styles.container}>
-            <TouchableOpacity
-                activeOpacity={0.7}
-                onPress={onPress}
-            >
-                <View style={[styles.card, { borderColor: bgColor }]}>
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity activeOpacity={0.7} onPress={onPress}>
+        <View style={[styles.card, { borderColor: bgColor }]}>
+          <Text style={[styles.idText, { color: bgColor }]}>
+            #{id < 10 ? "00" + id : id < 100 ? "0" + id : id}
+          </Text>
 
-                    <Text style={[styles.idText, { color: bgColor }]}>
-                        #{id < 10 ? '00' + id : id < 100 ? '0' + id : id}
-                    </Text>
-
-                    <Image
-                        source={{uri: front_default}}
-                        style={styles.img}
-                    />
-                    <View style={[styles.rectangle, { backgroundColor: bgColor }]}>
-                        <Text style={styles.text}>{name}</Text>
-                    </View>
-
-                </View>
-            </TouchableOpacity>
+          <Image source={{ uri: front_default }} style={styles.img} />
+          <View style={[styles.rectangle, { backgroundColor: bgColor }]}>
+            <Text style={styles.text}>{name}</Text>
+          </View>
         </View>
-    )
+      </TouchableOpacity>
+    </View>
+  );
 }
